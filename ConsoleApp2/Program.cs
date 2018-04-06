45bb75f8255e70b0b9e8ec77e8e4d471214c020f
Elton <shyt320@126.com>
2018-04-06 15:58:37 +0800

    第一次提交

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("你好，世界");
+            Console.ReadKey();
+     
+        }
+    }
+}
